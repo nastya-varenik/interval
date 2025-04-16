6821cac050b01d33c593d6add2ffb80d24fb6b82
Nastya Verenich <nastushka8921@gmail.com>
2025-04-16 15:08:42 +0300

    Delete src directory

--- src/main/java/by/verenich/Intervalproject/creator/IntervalFactory.java
+++
@@ -1,7 +0,0 @@
-package by.verenich.Intervalproject.creator;
-
-import by.verenich.Intervalproject.entity.Interval;
-
-public interface IntervalFactory {
-    Interval create(double start, double end, boolean includeStart, boolean includeEnd);
-}
--- src/main/java/by/verenich/Intervalproject/validator/IntervalValidator.java
+++
@@ -1,7 +0,0 @@
-package by.verenich.Intervalproject.validator;
-
-import by.verenich.Intervalproject.entity.Interval;
-
-public interface IntervalValidator {
-    boolean isValid(Interval interval);
-}
